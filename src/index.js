67f7b6dd42d86e22e97d2c5fc48d04da806cbca9
Asif Akbar <786asifakbar53@gmail.com>
2025-10-29 23:27:41 +0500

    database connetion succesfully

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv"
+import connectDb from './db/index.js'
+dotenv.config({
+    path : "./env"
+})
+
+
+
+connectDb();
+
+
+
+
+/*
+
+
+//ak Treqa ye hai database likhny ka 
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+import express from 'express';
+const app = express();
+
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       // ye app.on code lagany ka mqsad hai k kiya express database se connect hai use bat kar sakty hai   
+       app.on("error",(error)=>{
+            console.log("error" , error);
+            throw error
+         })
+         app.listen(process.env.PORT , ()=>{
+        console.log(`Port is listening ${process.env.PORT}`)
+         })
+    }catch(error){
+        console.error("error",error);
+        throw error
+        }
+})()
+        */
