6181b52b5d40243f1133347942623ba0b0fd8aed
Asif Akbar <786asifakbar53@gmail.com>
2025-10-30 12:42:52 +0500

    error handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requesthandler)=>{
+(req , res , next)=>{
+    Promise.resolve(requesthandler(req , res , next)).
+    catch((err)=> next(err)) 
+  }
+
+}
+
+export default asyncHandler;
+
+// ye tareqa bhi hai 
+// asyncHandler = (func) =>async (req , res ,next)=>{
+//     try {
+//         await func(req , res , next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             massage : error.massage,
+//         })
+//     }
+// }
