a739ad7d15985ce891d2e8e09b207341a87dc1dc
Asif Akbar <786asifakbar53@gmail.com>
2025-10-31 21:18:23 +0500

    bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,5 +49,16 @@ const userSchema = new Schema({
 
 },{timestamps : true});
 
+// ye code keh raha hai k save karny se pehly me password ko encrypt karna chah raha hu
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password"))return next();
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+});
+
+//ye code hai k user jo bar password enter karega usko encrypt karega 
+userSchema.methods.isPasswordCorrect = async function(password){
+return await bcrypt.compare(password , this.password)
+}
 
 export const User = mongoose.model("User" , userSchema);
