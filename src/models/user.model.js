c50efdcf0ab965b7afa6a4fc65858b72820eca12
Asif Akbar <786asifakbar53@gmail.com>
2025-11-01 10:34:36 +0500

    changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,97 +1,97 @@
-import mongoose , {schema}  from 'mongoose';
-import jwt  from 'jsonwebtoken';
+import mongoose, { schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 const userSchema = new Schema({
-    username :{
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true,
-        index : true,
-    },   
-    email :{
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
     },
-    fullname :{
-        type : String,
-        required : true,
-        index : true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
-    avatar :{
-        type : String, //cloudnery
+    fullname: {
+        type: String,
+        required: true,
+        index: true,
     },
-    coverImage :{
-        type : String, //cloudnery 
+    avatar: {
+        type: String, //cloudnery
     },
-    password:{
-        type : String,
-        required : [true , "Password is Required"],
-        
+    coverImage: {
+        type: String, //cloudnery 
+    },
+    password: {
+        type: String,
+        required: [true, "Password is Required"],
+
     },
 
-     refreshToken :{
-        type : String,
-        required : true,
-        unique : true,
+    refreshToken: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    watchHistory : [
-         {
-            type : Schema.Types.ObjectId,
-            ref : "Video"
-         },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
 
     ],
 
-},{timestamps : true});
+}, { timestamps: true });
 
 // ye code keh raha hai k save karny se pehly me password ko encrypt karna chah raha hu
-userSchema.pre("save" , async function(next){
-    if(!this.isModified("password"))return next();
-    this.password = bcrypt.hash(this.password , 10)
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
     next()
 });
 
 //ye code hai k user jo bar password enter karega usko encrypt karega 
-userSchema.methods.isPasswordCorrect = async function(password){
-return await bcrypt.compare(password , this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
 }
 // ye code acces token OTP etc
-userSchema.methods.genrateAccessToken = function(){
+userSchema.methods.genrateAccessToken = function () {
     return jwt.sign(
         {
-        _id : this._id,
-        email: this.email,
-        username: this.username,
-        fullname : this.fullname,
-    },
-         process.env.ACCESS_TOKEN_SECRET,
-         {
-          expiresIn : process.env.ACCESS_TOKEN_EXPIRY
-         },
-         
-)
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        },
+
+    )
 },
 
-//ye code refresh OTP token ka hai  
-userSchema.methods.genrateRefreshToken = function(){
-    return jwt.sign(
-        {
-    
-            _id : this._id,
-     
-       },
+    //ye code refresh OTP token ka hai  
+    userSchema.methods.genrateRefreshToken = function () {
+        return jwt.sign(
+            {
 
-         process.env.REFRESH_TOKEN_SECRET,
-         {
-          expiresIn : process.env.REFRESH_TOKEN_EXPIRY
-         },
-         
-)
-}
+                _id: this._id,
 
-export const User = mongoose.model("User" , userSchema);
+            },
+
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            },
+
+        )
+    }
+
+export const User = mongoose.model("User", userSchema);
