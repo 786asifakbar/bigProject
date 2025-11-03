c25fe1a09ff931867b02a50bd2368f0f54b76a38
Asif Akbar <786asifakbar53@gmail.com>
2025-11-03 11:17:46 +0500

    setting postman

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,11 @@
 import asyncHandler from "../utils/asyncHandler";
 
-const registerUser = asyncHandler(async (req , res)=>{
-res.status(200).json({
-    massage : "Ok"
-   })
+ const registerUser = asyncHandler(async (req , res)=>{
+
+    // get user datail from frontend 
+ const {fullname , username , eamil , password} = req.body
+ console.log("email" , email); 
+
 });
 
-export default registerUser;
+export  {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { registerUser } from "../controllers/user.controllers";
+import  { registerUser }  from "../controllers/user.controllers";
 
 const router = Router();
 
