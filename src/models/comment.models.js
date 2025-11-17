ea4f3023a574cafa9d40928c577d13fa2592bfe9
Asif Akbar <786asifakbar53@gmail.com>
2025-11-17 21:36:43 +0500

    fix: ensure proper export of comment model and maintain schema structure

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -4,8 +4,9 @@ import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 const commentSchema = new Schema(
     {
 
-        
+
     }, 
     { timestamps: true });
 
     commentSchema.plugin(mongooseAggregatePaginate);
+    export const comment = mongoose.model("comment" , commentSchema);
